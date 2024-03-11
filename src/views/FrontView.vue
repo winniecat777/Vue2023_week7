70290908739e9e68ed79bf3085a39f1ea80a6829
winniecat777 <winniecat777@gmail.com>
2024-03-11 19:10:35 +0800

    first commit

---
+++ src/views/FrontView.vue
@@ -0,0 +1,12 @@
+<template>
+    <nav>
+        <RouterLink to="/">Home</RouterLink> |
+        <RouterLink to="/about">About</RouterLink> |
+        <RouterLink to="/products">產品列表</RouterLink> |
+        <RouterLink to="/cart">購物車</RouterLink> |
+        <RouterLink to="/admin/products">後台 購物車列表</RouterLink> |
+        <RouterLink to="/login">登入</RouterLink> |
+    </nav>
+    <h2>這是前台</h2>
+    <router-view></router-view>
+</template>
