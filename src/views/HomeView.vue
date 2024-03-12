f1931c2a5733aab489b2cfb966aad68c13e89b92
winniecat777 <winniecat777@gmail.com>
2024-03-12 16:02:54 +0800

    del

--- src/views/HomeView.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="home">
-    <h1>Hello, This is Home Page.</h1>
-    <hr>
-  </div>
-</template>
-
-<style scoped></style>
--- src/views/NotFound.vue
+++
@@ -1,4 +0,0 @@
-<template>
-    <h1>404</h1>
-    <p>這頁找不到囉~</p>
-</template>
