42d9d90a3c5027049cda47923d7764a461a0fcc5
winniecat777 <winniecat777@gmail.com>
2024-03-12 17:11:40 +0800

    Updates

---
+++ assets/toastMessage-EB0FMTIy.js
@@ -0,0 +1,1 @@
+import{s as o}from"./index-vWNHF2ZC.js";const h=o("toastMessage",{state:()=>({messages:[]}),actions:{toastShow(){setTimeout(()=>{this.messages.shift()},6e3)},clearToast(s){this.messages.splice(s,1)},pushMessage(s){const{style:e="success",title:t,content:a}=s;this.messages.push({style:e,title:t,content:a}),this.toastShow()}}});export{h as u};
